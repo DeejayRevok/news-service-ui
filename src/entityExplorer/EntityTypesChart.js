f938697c07fcb95af0de9f770879f79ecc84592b
DeejayRevok <seryi_one@hotmail.com>
2020-06-07 18:56:09 +0200

    Add sentiment comparision pie

--- src/entityExplorer/EntityTypesChart.js
+++ src/entityExplorer/EntityTypesChart.js
@@ -1,8 +1,9 @@
 import * as React from "react";
-import {Cell, Pie, PieChart, ResponsiveContainer, Sector, Tooltip} from "recharts";
+import {Cell, Pie, PieChart, ResponsiveContainer, Tooltip} from "recharts";
 import entities from '../lib/Entities';
 import {Card} from "react-bootstrap";
 import './EntityTypesChart.css';
+import {renderActiveShape} from "../lib/GraphUtils";
 
 export class EntityTypesChart extends React.Component{
 
@@ -14,52 +15,6 @@ export class EntityTypesChart extends React.Component{
         { name: 'DATE', value: 100 },
         { name: 'MONEY', value: 150 },
     ]
-
-    renderActiveShape = (props) => {
-        const RADIAN = Math.PI / 180;
-        const {
-            cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
-            fill, payload, percent, value,
-        } = props;
-        const sin = Math.sin(-RADIAN * midAngle);
-        const cos = Math.cos(-RADIAN * midAngle);
-        const sx = cx + (outerRadius + 10) * cos;
-        const sy = cy + (outerRadius + 10) * sin;
-        const mx = cx + (outerRadius + 30) * cos;
-        const my = cy + (outerRadius + 30) * sin;
-        const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-        const ey = my;
-        const textAnchor = cos >= 0 ? 'start' : 'end';
-
-        return (
-            <g>
-                <Sector
-                    cx={cx}
-                    cy={cy}
-                    innerRadius={innerRadius}
-                    outerRadius={outerRadius}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    fill={fill}
-                />
-                <Sector
-                    cx={cx}
-                    cy={cy}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    innerRadius={outerRadius + 6}
-                    outerRadius={outerRadius + 10}
-                    fill={fill}
-                />
-                <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-                <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-                <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`PV ${value}`}</text>
-                <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-                    {`(Rate ${(percent * 100).toFixed(2)}%)`}
-                </text>
-            </g>
-        );
-    };
 
     render() {
         const chartCells = this.entitesData.map(entry => <Cell style={{border: '10px solid black'}} key={entry.name} fill={entities[entry.name]['color']} />)
@@ -73,7 +28,7 @@ export class EntityTypesChart extends React.Component{
                     <PieChart>
                         <Pie
                             activeIndex={activeIndex}
-                            activeShape={this.renderActiveShape}
+                            activeShape={renderActiveShape}
                             dataKey="value"
                             data={this.entitesData}
                             fill="#8884d8">
--- src/sentimentExplorer/SentimentPie.js
+++ src/sentimentExplorer/SentimentPie.js
@@ -1,16 +1,56 @@
 import * as React from "react";
 import {Card} from "react-bootstrap";
 import "./SentimentPie.css";
+import {Cell, Pie, PieChart, ResponsiveContainer, Tooltip} from "recharts";
+import {renderActiveShape} from "../lib/GraphUtils";
 
-export class SentimentPie extends React.Component{
+export class SentimentPie extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.onClickCell = this.onClickCell.bind(this);
+    }
+
+    sentimentData = [
+        {name: 'Positive', value: 400},
+        {name: 'Negative', value: 300}
+    ]
+
+    state = {
+        activeLabel: 'Positive'
+    }
+
+    onClickCell(label){
+        this.setState((state) =>{
+           state.activeLabel = label;
+           return state;
+        });
+    }
 
     render() {
+        const chartCells = this.sentimentData.map(entry => <Cell style={{border: '10px solid black'}}
+                                                                 key={entry.name}
+                                                                 fill={(entry.name === 'Positive') ? 'green' : 'red'}
+                                                                 onClick={() => this.onClickCell(entry.name)}/>)
+        const activeIndex = this.sentimentData.findIndex(entry => entry.name === this.state.activeLabel);
         return <Card className="SentimentPie">
             <Card.Header style={{backgroundColor: 'silver'}}>
                 Sentiments comparision
             </Card.Header>
             <Card.Body>
-
+                <ResponsiveContainer>
+                    <PieChart>
+                        <Pie
+                            activeIndex={activeIndex}
+                            activeShape={renderActiveShape}
+                            dataKey="value"
+                            data={this.sentimentData}
+                            fill="#8884d8">
+                            {chartCells}
+                        </Pie>
+                        <Tooltip/>
+                    </PieChart>
+                </ResponsiveContainer>
             </Card.Body>
         </Card>
     }
